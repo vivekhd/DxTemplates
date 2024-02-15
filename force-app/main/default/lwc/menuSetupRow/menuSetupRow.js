88fbdae6b90ef83deb08f3cb795b962dc96d8299
rahulb-dx <rahulb@dextara.com>
2024-02-15 16:08:34 +0530

    Clean-Up Action Items - Rahul

--- force-app/main/default/lwc/menuSetupRow/menuSetupRow.js
+++ force-app/main/default/lwc/menuSetupRow/menuSetupRow.js
@@ -4,13 +4,9 @@ export default class DxCpqMenuSetupRow extends LightningElement {
     @api myWrapper;
     isExpanded;
     selectedRecordId;
+    
     connectedCallback(){
-        this.isExpanded = this.myWrapper.isExpanded;
-    }
-    toggleView(event){
-        const selectedEvent = new CustomEvent("toggle",{bubbles: true, composed:true, detail:{_index:this.myWrapper._index, recordId : this.myWrapper.Id, isExpanded: this.myWrapper.isExpanded}});
-        this.dispatchEvent(selectedEvent);
-        this.isExpanded = !this.isExpanded;
+        this.isExpanded = false;
     }
 
     chooseWrapper(event){
